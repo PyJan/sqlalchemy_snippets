a9519b253283ae332fa76b5c8f8627d960ef7862
PyJan <PyJan3@gmail.com>
2018-11-07 23:44:38 +0100

    login form

---
+++ logins.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 22:25:47 2018
+
+@author: jan
+"""
+from flask import Flask, render_template, request
+from wtforms import Form, StringField
+
+app = Flask(__name__)
+
+class Userform(Form):
+    login = StringField('login')
+    password = StringField('password')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        userform = Userform(request.form)
+        print(userform.login.data, userform.password.data)
+    else:
+        userform = Userform()
+    return render_template('main.html', userform=userform)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
