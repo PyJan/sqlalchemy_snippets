a93c6f5f7bef8d82cc12f8cf7c2653364ea4f309
PyJan <jan.svarcbach@gmail.com>
2018-11-16 22:44:13 +0100

    deletion from flex db

--- flextable.py
+++ flextable.py
@@ -1,19 +1,29 @@
 from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flex.db'
+db = SQLAlchemy(app)
 
-x = ['prvni', 'druhy', 'treti', 'ctvrty']
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String, nullable = False)
+
+    def __repr__(self):
+        return '<Item {}>'.format(self.item)
+
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
     if request.method == 'POST':
-        print(request.form['name'])
+        db.session.delete(Items.query.filter_by(item=request.form['name']).first())
+        db.session.commit()
     return redirect(url_for('main'))
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('flextable.html', x=x)
+    return render_template('flextable.html', x=[x.item for x in Items.query.all()])
 
 if __name__ == '__main__':
     app.run(debug=True)
